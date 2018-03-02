719bb02cc02026600cbe9fd33d52a94e964649f2
AKouma <elbacar08@gmail.com>
2018-03-02 03:01:07 +0100

    init

---
+++ app/src/main/java/com/example/akoumare/testandroidnatif/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.akoumare.testandroidnatif;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ListView list = (ListView)findViewById(R.id.maliste);
+        ArrayAdapter<String> tableau = new ArrayAdapter<String>(list.getContext(), R.layout.ligne, R.id.monTexte);
+        for (int i=0; i<40; i++) {
+            tableau.add("film " + i);
+        }
+        list.setAdapter(tableau);
+    }
+}

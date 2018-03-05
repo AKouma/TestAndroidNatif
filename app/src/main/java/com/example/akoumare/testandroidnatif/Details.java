07dc032ae6eb259b51f74b9b7eb46aefd26fbfd1
AKouma <elbacar08@gmail.com>
2018-03-05 18:38:25 +0100

    loader non

---
+++ app/src/main/java/com/example/akoumare/testandroidnatif/Details.java
@@ -0,0 +1,89 @@
+package com.example.akoumare.testandroidnatif;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+public class Details extends Activity {
+
+    ImageView image;
+    ListView list;
+    Button retour;
+    Intent intent;
+    String titre =null;
+    String annee =null;
+    String acteurs =null;
+    String genre =null;
+    String lien=null;
+    ArrayAdapter tabl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+
+        image = (ImageView) findViewById(R.id.imageFill);
+        list = (ListView) findViewById(R.id.detailFilm);
+        retour =(Button)findViewById(R.id.retour);
+
+        tabl =new ArrayAdapter<String>(this, R.layout.details,R.id.detailFilm);
+
+        retour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               finish();
+            }
+        });
+
+        intent = getIntent();
+        titre = intent.getStringExtra("titre");
+        annee = intent.getStringExtra("annee");
+        acteurs = intent.getStringExtra("acteurs");
+        genre = intent.getStringExtra("genre");
+        lien= intent.getStringExtra("lien");
+
+        Toast.makeText(this,lien.toString(),Toast.LENGTH_SHORT).show();
+
+        Picasso.with(getBaseContext()).load(lien.toString()).into(image);
+
+      /*  if(!titre.isEmpty() && titre  !=null){
+            tabl.add("Titre :"+titre);
+        }
+        else {
+            Toast.makeText(this,"bien",Toast.LENGTH_SHORT).show();
+        }
+
+        if(!annee.isEmpty() && annee  !=null){
+            tabl.add("Année de sortie :"+annee);
+        }
+        else {
+            Toast.makeText(this,"bien",Toast.LENGTH_SHORT).show();
+        }
+
+
+        if(!acteurs.isEmpty() && acteurs  !=null){
+            tabl.add("Acteurs :"+acteurs);
+        }
+        else {
+            Toast.makeText(this,"bien",Toast.LENGTH_SHORT).show();
+        }
+
+
+        if(!genre.isEmpty() && genre  !=null){
+            tabl.add("Genre :"+genre);
+        } else {
+            Toast.makeText(this,"bien",Toast.LENGTH_SHORT).show();
+        }
+
+*/
+
+    }
+}
